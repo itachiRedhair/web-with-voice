4356ff2042127930c38def4359d859ea01bb6d22
itachiRedhair <akshay.milmile@gmail.com>
2018-06-14 00:05:33 +0530

    Initial Commit

---
+++ google-actions/config.js
@@ -0,0 +1,3 @@
+const owAPIKey = process.env.OW_API_Key || "4bab2df8f5a678cbe86ac01e5203de87";
+
+module.exports = { owAPIKey };
--- google-actions/index.js
+++ google-actions/index.js
@@ -1,6 +1,7 @@
 const expressApp = require("express")();
 const bodyParser = require("body-parser");
 const server = require("http").Server(expressApp);
+const fetch = require("node-fetch");
 
 const io = require("socket.io")(server);
 
@@ -9,6 +10,10 @@ const { dialogflow } = require("actions-on-google");
 // socket instance holder
 let singleSocket;
 
+// Open Weather URL
+const apiKey = require("./config").owAPIKey;
+const openWeatherURL = `http://api.openweathermap.org/data/2.5/weather?appid=${apiKey}`;
+
 // Create an dialogflow app instance
 const dialogflowApp = dialogflow();
 expressApp.use(bodyParser.json(), dialogflowApp);
@@ -16,14 +21,28 @@ expressApp.use(bodyParser.json(), dialogflowApp);
 // Register handlers for Dialogflow intents
 
 dialogflowApp.intent("Default Welcome Intent", conv => {
-  conv.ask("Hi, how is it going?");
+  conv.ask("Hey, hello there. Try asking weather in Pune.");
 });
 
-// Intent in Dialogflow called `HelloWorld`
-dialogflowApp.intent("HelloWorld", conv => {
-  const messageToSend = `Raw Text: ${conv.input.raw}, Intent: ${conv.intent}`;
-  singleSocket.emit("message", messageToSend);
-  conv.ask("Hello Akshay");
+// Intent in Dialogflow called `AskTemperature`
+dialogflowApp.intent("AskTemperature", async (conv, { city, country }) => {
+  const location = city ? city : country;
+  const response = await fetch(`${openWeatherURL}&q=${location}`);
+  const data = await response.json();
+  const { weather, main } = data;
+  const messageToSend = {
+    city: location,
+    condition: weather[0],
+    weather: main
+  };
+
+  singleSocket.emit("temperature-data", messageToSend);
+
+  conv.ask(
+    `Weather in ${location} is ${(main.temp - 273.15).toFixed(
+      2
+    )} degree celsius.`
+  );
 });
 
 io.on("connection", function(socket) {
