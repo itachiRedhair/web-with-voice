67f11d81f0943b2aeaa9b9875ff96c6254b24bea
itachiRedhair <akshay.milmile@gmail.com>
2018-06-14 00:06:31 +0530

    first commit

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,34 +1,82 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 import io from "socket.io-client";
 const socket = io("http://localhost:3001");
+
+const DataTable = ({ city, condition, weather }) => (
+  <section>
+    <h1> Temperature Details For "{city}" </h1>
+    <div class="tbl-content">
+      <table cellPadding="0" cellSpacing="0" border="0">
+        <tbody>
+          <tr>
+            <td> Forecast Summary </td>
+            <td> {condition.main} </td>
+          </tr>
+          <tr>
+            <td> Forecast Details </td>
+            <td> {condition.description} </td>
+          </tr>
+          <tr>
+            <td> Temperature </td>
+            <td>
+              {(weather.temp - 273.15).toFixed(2)+' '}
+              <sup>o</sup>
+              C
+            </td>
+          </tr>
+          <tr>
+            <td> Humidity </td>
+            <td> {weather.humidity}% </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+);
 
 class App extends Component {
   constructor() {
     super();
 
     socket.on("connection-response", data => {
-      this.setState({ message: JSON.stringify(data) });
+      if (data) {
+        this.setState({ connectionStatus: "Connection Established" });
+      }
     });
 
-    socket.on("message", data => {
-      this.setState({ message: data });
+    socket.on("temperature-data", weatherData => {
+      this.setState({ weatherData });
     });
 
     this.state = {
-      message: "nothing"
+      connectionStatus: "Connection Not Established.",
+      weatherData: {
+        city: null,
+        condition: null,
+        weather: null
+      }
     };
   }
   render() {
+    const { city, condition, weather } = this.state.weatherData;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img
+            src={require("./assets/logo2.png")}
+            className="App-logo"
+            alt="logo"
+          />
+          <h1 className="App-title">
+            Ask Google "What's the weather in Pune?"
+          </h1>
         </header>
-        <p className="App-intro">Message: {this.state.message}</p>
+        <p className="App-intro">{this.state.connectionStatus}</p>
+        {city !== null ? (
+          <DataTable city={city} condition={condition} weather={weather} />
+        ) : null}
       </div>
     );
   }
